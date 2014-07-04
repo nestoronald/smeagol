e843a6e90f09c633f26b37f172641c897241ae82
Ronald_Adiestra <ronald4261@hotmail.com>
2014-07-04 03:50:05 -0500

    view table of database

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -14,8 +14,23 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+	// agregamos este atributo
+	protected $nodeTable;
+	
     public function indexAction()
     {
-        return new ViewModel();
+        return new ViewModel(array(
+            'nodes' => $this->getNodeTable()->fetchAll(),
+        ));
+    }
+    
+    // Agregamos este método
+    public function getNodeTable()
+    {
+    	if (!$this->nodeTable) {
+    		$sm = $this->getServiceLocator();
+    		$this->nodeTable = $sm->get('Smeagol\Model\NodeTable');
+    	}
+    	return $this->nodeTable;
     }
 }
