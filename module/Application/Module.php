bfa4aa3813d374c17384eb27146e0ad869c8efc0
Ronald_Adiestra <ronald4261@hotmail.com>
2014-08-12 22:17:17 -0500

    rules fin de curso en adiestra

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -30,10 +30,11 @@ use Zend\Authentication\AuthenticationService;
 use Zend\Permissions\Acl\Acl;
 use Zend\Permissions\Acl\Role\GenericRole;
 use Zend\Permissions\Acl\Resource\GenericResource;
+use Smeagol\Rules\NodeRule;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
 
-    //.. Ahora modificamos el método onBootStrap
+//.. Ahora modificamos el método onBootStrap
     public function onBootstrap(MvcEvent $e) {
         $e->getApplication()->getServiceManager()->get('translator');
         $eventManager = $e->getApplication()->getEventManager();
@@ -47,10 +48,10 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
         $eventManager->attach('route', function($e) {
-            // rol por defecto
+// rol por defecto
             $userRole = 'guest';
 
-            // verificando si el usuario esta logueado
+// verificando si el usuario esta logueado
             $auth = new AuthenticationService();
             $is_login = false;
             if ($auth->hasIdentity()) {
@@ -62,8 +63,8 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
             $roleTable = $e->getApplication()->getServiceManager()->get('Smeagol\Model\RoleTable');
             $roles = $roleTable->fetchAll();
 
-            // Datos se obtendrán de la base de datos
-            // $roles = array('guest', 'member', 'editor', 'admin');
+// Datos se obtendrán de la base de datos
+// $roles = array('guest', 'member', 'editor', 'admin');
 
             $rolePermissionTable = $e->getApplication()->getServiceManager()->get('Smeagol\Model\RolePermissionTable');
             $roles_permissions = $rolePermissionTable->fetchAll();
@@ -88,15 +89,15 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
               )
               );
              */
-            // Instanciando la clase Acl
+// Instanciando la clase Acl
             $acl = new Acl();
 
-            // Agregando los roles
+// Agregando los roles
             foreach ($roles as $role) {
                 $acl->addRole(new GenericRole($role->type));
             }
 
-            // Obteniendo los módulos del sistema
+// Obteniendo los módulos del sistema
             $manager = $e->getApplication()->getServiceManager()->get('ModuleManager');
             $modules = $manager->getLoadedModules();
             $loadedModules = array_keys($modules);
@@ -122,11 +123,12 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
                             if (substr($action, strlen($action) - 6) === 'Action' && !in_array($action, $skipActionsList)) {
                                 $action = strtolower(substr($action, 0, -6));
                                 $resource = "mvc:$module.$controller.$action";
-                                // agregando todos los actions como recursos de la aplicación
+// agregando todos los actions como recursos de la aplicación
                                 $acl->addResource(new GenericResource($resource));
 
-                                // recorriendo el array de permisos para asignar acceso a los recursos
+// recorriendo el array de permisos para asignar acceso a los recursos
                                 foreach ($permissions as $role => $permission) {
+
                                     $is_allowed = false;
                                     if (in_array("mvc:$module.*", $permission)) {
                                         $is_allowed = true;
@@ -140,7 +142,31 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
                                         $is_allowed = true;
                                     }
 
-                                    // asignado permiso de acceso al recurso
+                                    if (in_array("$resource:owner", $permission)) {
+
+                                        if ($is_login) {
+                                            $routeParams = $e->getRouteMatch()->getParams();
+                                            $scontroller = trim($routeParams['controller']);
+                                            $rs = explode("\\", $scontroller);
+                                            $moduleR = strtolower($rs[0]);
+                                            $controllerR = strtolower($rs[2]);
+                                            $actionR = strtolower($routeParams['action']);
+
+                                            if ($resource == "mvc:$moduleR.$controllerR.$actionR") {
+
+                                                if (isset($routeParams['id'])) {
+                                                    $nodeid = (int) $routeParams['id'];
+                                                    $nodeRule = $e->getApplication()->getServiceManager()->get('Smeagol\Rules\NodeRule');
+
+                                                    if ($nodeRule->userIsOwner($nodeid, $identity->id)) {
+                                                        $is_allowed = true;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+
+// asignado permiso de acceso al recurso
                                     if ($is_allowed) {
                                         $acl->allow($role, $resource);
                                     }
@@ -151,36 +177,39 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
                 }
             }
 
-            // validamos si el rol tiene acceso a este recurso
+// validamos si el rol tiene acceso a este recurso
             $routeParams = $e->getRouteMatch()->getParams();
             $scontroller = trim($routeParams['controller']);
             $rs = explode("\\", $scontroller);
             $module = strtolower($rs[0]);
             $controller = strtolower($rs[2]);
             $action = strtolower($routeParams['action']);
+            if (isset($routeParams['id'])) {
+                $id = (int) $routeParams['action'];
+            }
 
             $acceso = false;
 
-            // verificación de acceso al recurso
+// verificación de acceso al recurso
             $resource = "mvc:$module.$controller.$action";
             if ($acl->isAllowed($userRole, $resource)) {
                 $acceso = true;
             }
 
-            // Bloqueo de acceso
+// Bloqueo de acceso
             if (!$acceso) {
                 die("acceso denegado");
             }
 
-            // validamos si entramos en el index del portal
+// validamos si entramos en el index del portal
             $is_front = false;
-            // obtenemos la ruta del request
+// obtenemos la ruta del request
             $ruta = $e->getRouter()->getRequestUri()->getPath();
 
             if ($ruta == "/" || $ruta == "/application" || $ruta === "/application/index" || $ruta === "/application/index/index") {
                 $is_front = true;
             }
-            // decide which theme to use by get parameter
+// decide which theme to use by get parameter
             $layout = 'enterprise/layout';
             $e->getViewModel()->setTemplate($layout);
             $e->getViewModel()->setVariable("is_login", $is_login);
@@ -205,7 +234,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
         );
     }
 
-    // Add this method:
+// Add this method:
     public function getServiceConfig() {
         return array(
             'factories' => array(
@@ -267,6 +296,14 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
             $resultSetPrototype = new ResultSet();
             $resultSetPrototype->setArrayObjectPrototype(new Menu());
             return new TableGateway('menu', $dbAdapter, null, $resultSetPrototype);
+        },
+                'Smeagol\Rules\NodeRule' => function($sm) {
+
+            $tableGateway = $sm->get('NodeTableGateway');
+
+            $table = new NodeRule($tableGateway);
+
+            return $table;
         },
             ),
         );
